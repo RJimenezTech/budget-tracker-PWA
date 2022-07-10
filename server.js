447090e909f99ac5ca218418923b58da4d1cfd97
Robert Jimenez <robertjimenez86@gmail.com>
2022-07-10 12:46:03 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://localhost:27017/rjimenez-budget-tracker",
+  {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
