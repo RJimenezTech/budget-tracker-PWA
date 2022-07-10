688fd3bb42344bb368a4d57ef6242443d30fe534
Robert Jimenez <robertjimenez86@gmail.com>
2022-07-10 12:54:25 -0500

    fixed webpack and double MONGODB-URI issue

--- server.js
+++ server.js
@@ -16,15 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://localhost:27017/rjimenez-budget-tracker",
-  {
-    useNewUrlParser: true,
-    // useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  // useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 // routes
 app.use(require("./routes/api.js"));
